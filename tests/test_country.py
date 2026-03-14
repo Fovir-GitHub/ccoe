17ac7365712d36efdc70d2d7e7255372d7f13ff0
Fovir <fovir@disroot.org>
2026-03-14 19:54:19 +0800

    test(normalization): add tests for normalization functions

---
+++ tests/test_country.py
@@ -0,0 +1,17 @@
+import unittest
+from src.normalization.country import normalize_country
+
+
+class TestNormalizeCountry(unittest.TestCase):
+    def test_valid_country(self):
+        self.assertEqual(normalize_country("MY"), "MY")
+        self.assertEqual(normalize_country("sg"), "SG")
+
+    def test_invalid_country(self):
+        DEFAULT_COUNTRY = "MY"
+        self.assertEqual(normalize_country(""), DEFAULT_COUNTRY)
+        self.assertEqual(normalize_country("XX"), DEFAULT_COUNTRY)
+
+
+if __name__ == "__main__":
+    unittest.main()
